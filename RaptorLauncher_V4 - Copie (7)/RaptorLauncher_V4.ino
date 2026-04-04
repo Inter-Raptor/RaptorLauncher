c8aec183bc6f1248e4086c8f33434af35a875a33
Inter-Raptor <VivienJardotID@outlook.com>
2026-04-04 18:40:23 +0200

    ok

---
+++ RaptorLauncher_V4 - Copie (7)/RaptorLauncher_V4.ino
@@ -0,0 +1,48 @@
+#include "config.h"
+#include "types.h"
+#include "display_manager.h"
+#include "touch_manager.h"
+#include "storage_manager.h"
+#include "settings_manager.h"
+#include "audio_manager.h"
+#include "input_manager.h"
+#include "launcher_ui.h"
+#include "wifi_manager.h"
+#include "led_manager.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+
+  Serial.println();
+  Serial.println("=== RaptorLauncher V4 ===");
+
+  displayInit();
+  storageInit();
+
+  settingsInit();
+
+  touchInit();
+  displaySetBrightness(map(settingsGet().brightness, 0, 100, 10, 255));
+
+  audioInit();
+  audioSetVolume(settingsGet().volume);
+  ledManagerInit();
+  ledManagerSetBrightness(settingsGet().led_brightness);
+
+  wifiManagerInit();
+  
+  inputInit();
+  launcherInit();
+
+  Serial.println("Setup termine");
+}
+
+void loop() {
+  wifiManagerUpdate();
+  touchUpdate();   // <<< IMPORTANT : mise a jour tactile
+  inputUpdate();
+  launcherUpdate();
+  launcherRender();
+  delay(16);
+}
