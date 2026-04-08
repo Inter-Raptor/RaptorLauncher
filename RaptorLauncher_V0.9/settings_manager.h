f168c15bad4fd416f41fad9cada63839b3298dc9
Inter-Raptor <VivienJardotID@outlook.com>
2026-04-08 15:45:26 +0200

    feat: add configurable boot splash and home background in v0.9

--- RaptorLauncher_V0.9/settings_manager.h
+++ RaptorLauncher_V0.9/settings_manager.h
@@ -4,9 +4,12 @@
 struct SystemSettings {
   int volume;       // 0 à 100
   int brightness;   // 0 à 100
+  int boot_splash_ms; // 0 à 10000
 
   String wifi_ssid;
   String wifi_pass;
+  String boot_splash_raw; // image RAW plein ecran au demarrage
+  String home_bg_raw;     // image RAW fond ecran accueil
 
   int touch_x_min;
   int touch_x_max;
