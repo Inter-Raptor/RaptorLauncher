d86928d1a46a5a04dfd1f379a2a87322b29bc04c
Inter-Raptor <VivienJardotID@outlook.com>
2026-04-10 11:59:25 +0200

    ok

--- RaptorLauncher_V0.9/game_boot_manager.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool gameBootInit();
-bool gameBootLaunchFromPath(const String& binPath);
-bool gameBootMarkReturnToLauncher();
--- RaptorLauncher_V0.9/led_manager.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void ledManagerInit();
-void ledManagerSetBrightness(int percent);
-void ledManagerSetColor(uint8_t r, uint8_t g, uint8_t b);
-void ledManagerOff();
