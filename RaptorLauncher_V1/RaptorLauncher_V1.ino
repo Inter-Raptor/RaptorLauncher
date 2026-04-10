c1768ee3652bcb2fad33fd768f9f19831a1e72b0
Inter-Raptor <VivienJardotID@outlook.com>
2026-04-10 13:56:30 +0200

    ok

---
+++ RaptorLauncher_V1/RaptorLauncher_V1.ino
@@ -0,0 +1,56 @@
+#include "config.h"
+#include "types.h"
+#include "display_manager.h"
+#include "touch_manager.h"
+#include "storage_manager.h"
+#include "settings_manager.h"
+#include "audio_manager.h"
+#include "input_manager.h"
+#include "launcher_ui.h"
+#include "wifi_manager.h"
+#include "led_manager.h"
+#include "mcp23017_manager.h"
+#include "game_boot_manager.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+
+  Serial.println();
+  Serial.println("=== RaptorLauncher V1 ===");
+
+  displayInit();
+  storageInit();
+
+  if (!displayDrawRAW("/boot.raw", 0, 0, 320, 240)) {
+    displayDrawRAW("/boot.bg.raw", 0, 0, 320, 240);
+  }
+
+  settingsInit();
+
+  touchInit();
+  displaySetBrightness(map(settingsGet().brightness, 0, 100, 10, 255));
+
+  audioInit();
+  audioSetVolume(settingsGet().volume);
+  ledManagerInit();
+  ledManagerSetBrightness(settingsGet().led_brightness);
+  mcp23017Init();
+  gameBootInit();
+
+  wifiManagerInit();
+  
+  inputInit();
+  launcherInit();
+
+  Serial.println("Setup termine");
+}
+
+void loop() {
+  wifiManagerUpdate();
+  touchUpdate();   // <<< IMPORTANT : mise a jour tactile
+  inputUpdate();
+  launcherUpdate();
+  launcherRender();
+  delay(16);
+}
